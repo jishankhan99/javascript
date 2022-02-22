9a96741ce28a1c5eec526c7cb01100543589c54d
jishankhan99 <96298175+jishankhan99@users.noreply.github.com>
2022-02-22 18:44:07 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,64 @@
+"use strict";
+let myname ="aksh";
+let myfriendname ="bksh personal account";
+
+alert(myname);
+alert(myfriendname);
+alert("hello simu")
+
+let price= true;
+//bolean/bool
+true/false
+ 
+let name =prompt("Enter your name");
+let isAdmin = confirm("are you admin");
+console.log(typeof isAdmin);
+//Increment And Decrement Operators 
+let num = 5;
+num++;
+num++;
+num--;
+alert(num);
+//eternal snap gain farm long belt latin inspire execute doctor north since       its my metamask account code
+
+
+let amount = 21;
+if (amount == 10){//false
+    alert("10");
+}
+else if (amount == 20){//false
+alert("20");
+}else
+ {alert("something");
+
+ }
+
+
+
+ for (let count= 1; count <=5; count++)
+{
+    console.log("list "+ count);
+
+for (let usercount = 1; usercount <=3; usercount++)
+{
+    console.log( "---------user" + usercount);
+}
+
+
+
+
+function test(memberid,MEMBERAGE){
+    return AbortController(memberid,memberAge);
+}
+function showMyFullname(){
+    return"raj";
+}
+let myname =getmyfullname();
+function isvalidMember(memberid){
+
+}
+function showMyname ( mynameNickname){
+    alert(myNickname);
+}
+
+
